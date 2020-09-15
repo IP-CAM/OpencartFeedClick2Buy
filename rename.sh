5b3991de1fd6c37fc92dc5f4841a8038a4267429
Keith <keith@vool.ie>
2020-09-16 00:14:14 +0100

    Clean up

--- rename.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-find . -type f -name 'click2buy_csv*' | while read FILE ; do
-    newfile="$(echo ${FILE} |sed -e 's/\click2buy_csv/click2buy_csv/')" ;
-    mv "${FILE}" "${newfile}" ;
-done
